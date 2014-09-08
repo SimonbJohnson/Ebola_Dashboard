5a25a23db78b4561007db74dab988833c23b43a2
SimonbJohnson <simonbjohnson@gmail.com>
2014-09-08 17:54:10 +0100

    Updated key stats for 5th sept

--- data/KeyStats.js
+++ data/KeyStats.js
@@ -1,6 +1,6 @@
-var keyStats = {	"Total":{	"population":"190.5",	"cases":"3706",	"deaths":"1847",	"mortality rate":"50%"	},
-	"Guinea":{	"population":"11.5",	"cases":"771",	"deaths":"494",	"mortality rate":"64%"	},
-	"Liberia":{	"population":"4.2",	"cases":"1698",	"deaths":"871",	"mortality rate":"51%"	},
-	"Sierra Leone":{	"population":"6",	"cases":"1216",	"deaths":"476",	"mortality rate":"39%"	},
-	"Nigeria":{	"population":"168.8",	"cases":"21",	"deaths":"6",	"mortality rate":"29%"	}
+var keyStats = {	"Total":{	"population":"190.5",	"cases":"3967",	"deaths":"2105",	"mortality rate":"53%"	},
+	"Guinea":{	"population":"11.5",	"cases":"812",	"deaths":"517",	"mortality rate":"64%"	},
+	"Liberia":{	"population":"4.2",	"cases":"1871",	"deaths":"1089",	"mortality rate":"58%"	},
+	"Sierra Leone":{	"population":"6",	"cases":"1261",	"deaths":"491",	"mortality rate":"39%"	},
+	"Nigeria":{	"population":"168.8",	"cases":"22",	"deaths":"8",	"mortality rate":"36%"	}
 }						
