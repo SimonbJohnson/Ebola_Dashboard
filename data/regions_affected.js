e2efc57152cd195e7cf05a15f0216b8cc4f82a6a
SimonbJohnson <simonbjohnson@gmail.com>
2014-10-17 18:06:54 +0100

    Update affect regions for 15 Oct

--- data/regions_affected.js
+++ data/regions_affected.js
@@ -1,42 +1,48 @@
-var regionsAffected = [			
+var regionsAffected = [
+
 {"Region":"Conakry","Pcode":"GIN002001"},
-{"Region":"Kissidougou","Pcode":"GIN003004"},
-{"Region":"Macenta","Pcode":"GIN008004"},
-{"Region":"Guéckédou","Pcode":"GIN008002"},
-{"Region":"Pita","Pcode":"GIN007003"},
-{"Region":"Nzékékoré","Pcode":"GIN008005"},
-{"Region":"Yomou","Pcode":"GIN008006"},
-{"Region":"Forecariah","Pcode":"GIN005003"},
+
+
+
 {"Region":"Kerouane","Pcode":"GIN004002"},
+
+
 {"Region":"Coyah","Pcode":"GIN005001"},
+
+{"Region":"Forecariah","Pcode":"GIN005003"},
+
+
 {"Region":"Dalaba","Pcode":"GIN007001"},
-{"Region":"Kindia","Pcode":"GIN005005"},
-{"Region":"Lofa","Pcode":"LBR08"},
-{"Region":"Montserrado","Pcode":"LBR11"},
-{"Region":"Margibi","Pcode":"LBR09"},
+
+{"Region":"Beyla","Pcode":"GIN008001"},
+{"Region":"Guéckédou","Pcode":"GIN008002"},
+{"Region":"Lola","Pcode":"GIN008002"},
+{"Region":"Macenta","Pcode":"GIN008004"},
+{"Region":"Nzékékoré","Pcode":"GIN008005"},
 {"Region":"Bomi","Pcode":"LBR01"},
-{"Region":"Nimba","Pcode":"LBR12"},
 {"Region":"Bong","Pcode":"LBR02"},
 {"Region":"Grand Bassa","Pcode":"LBR04"},
 {"Region":"Grand Cape Mount","Pcode":"LBR05"},
-{"Region":"River Cess","Pcode":"LBR14"},
-{"Region":"Grand Gedeh","Pcode":"LBR06"},
-{"Region":"River Gee","Pcode":"LBR13"},
-{"Region":"Sinoe","Pcode":"LBR15"},
-{"Region":"Gbarpolu","Pcode":"LBR03"},
-{"Region":"Maryland","Pcode":"LBR10"},
 {"Region":"Grand Kru","Pcode":"LBR07"},
+{"Region":"Lofa","Pcode":"LBR08"},
+{"Region":"Margibi","Pcode":"LBR09"},
+{"Region":"Maryland","Pcode":"LBR10"},
+{"Region":"Montserrado","Pcode":"LBR11"},
+{"Region":"Nimba","Pcode":"LBR12"},
+{"Region":"River Gee","Pcode":"LBR13"},
+{"Region":"River Cess","Pcode":"LBR14"},
+{"Region":"Sinoe","Pcode":"LBR15"},
 {"Region":"Kailahun","Pcode":"SLE0101"},
 {"Region":"Kenema","Pcode":"SLE0102"},
 {"Region":"Kono","Pcode":"SLE0103"},
-{"Region":"Kambia","Pcode":"SLE0202"},
 {"Region":"Bombali","Pcode":"SLE0201"},
-{"Region":"Tonkolili","Pcode":"SLE0205"},
+{"Region":"Kambia","Pcode":"SLE0202"},
 {"Region":"Port Loko","Pcode":"SLE0204"},
-{"Region":"Pujehun","Pcode":"SLE0304"},
+{"Region":"Tonkolili","Pcode":"SLE0205"},
 {"Region":"Bo","Pcode":"SLE0301"},
-{"Region":"Moyamba","Pcode":"SLE0303"},
 {"Region":"Bonthe","Pcode":"SLE0302"},
-{"Region":"Western Area Urban","Pcode":"SLE0402"},
-{"Region":"Western Area Rural","Pcode":"SLE0401"}
+{"Region":"Moyamba","Pcode":"SLE0303"},
+{"Region":"Pujehun","Pcode":"SLE0304"},
+{"Region":"Western Area Rural","Pcode":"SLE0401"},
+{"Region":"Western Area Urban","Pcode":"SLE0402"}
 ];
