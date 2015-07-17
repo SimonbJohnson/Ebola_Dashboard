09d705f409252cd5485a2d5a2313238f45c0f281
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2015-07-17 13:30:45 +0100

    Update parseDates

--- js/site.js
+++ js/site.js
@@ -620,7 +620,7 @@ var totalCasesByDate = byDate.group().reduceSum(function(d){return d.CumulativeC
 //var totalCasesByDate = byDate.group();
 var totalDeathsByDate = byDate.group().reduceSum(function(d){return d.CumulativeDeaths;});
 
-var lastWeeks = [parseDate("06/07/2015").valueOf(),parseDate("29/06/2015").valueOf()];
+var lastWeeks = [parseDate("13/07/2015").valueOf(),parseDate("06/07/2015").valueOf()];
 
 //helper function for formatting numbers with comma separator for thousands
 var formatComma = d3.format(",");
